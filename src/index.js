9ab7b4ee4e796b08d98a031d881733fbd5d13edf
Lucas Simidu <lucas.simidu@outlook.com>
2025-07-15 00:03:31 -0300

    feat: duplicated Words Verification function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ const fs = require('fs'); //file system
 const path = process.argv;
 const link = path[2];
 
-fs.readFile(link, 'utf-8', (erro, texto) => console.log(texto))
+fs.readFile(link, 'utf-8', (err, str) => duplicatedWordsVerification(str))
 
+function duplicatedWordsVerification(str) {
+    const wordsList = str.split(' ');
+    const result = {}
+    
+    wordsList.forEach(word => {
+        result[word] = (result[word] || 0) + 1
+    })
+    console.log(result);
+}
