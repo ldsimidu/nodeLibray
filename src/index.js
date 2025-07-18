3b973996ae1e993c2c64275c396e64adbcb35c35
Lucas Simidu <lucas.simidu@outlook.com>
2025-07-17 22:12:04 -0300

    feat: paragraph break function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,16 @@ const fs = require('fs'); //file system
 const path = process.argv;
 const link = path[2];
 
-fs.readFile(link, 'utf-8', (err, str) => duplicatedWordsVerification(str))
+fs.readFile(link, 'utf-8', (err, str) => paragraphBreak(str))
+
+function paragraphBreak(str) {
+    const paragraphList = str.toLowerCase().split('\n')
+    const count = paragraphList.map((paragraph) => {
+        return duplicatedWordsVerification(paragraph)
+    })
+    console.log(count);
+    
+}
 
 function duplicatedWordsVerification(str) {
     const wordsList = str.split(' ');
@@ -12,5 +21,5 @@ function duplicatedWordsVerification(str) {
     wordsList.forEach(word => {
         result[word] = (result[word] || 0) + 1
     })
-    console.log(result);
+    return result;
 }
