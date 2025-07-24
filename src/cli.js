fc71d9e447d4649ce5134533c1099f611d45664b
Lucas Simidu <lucas.simidu@outlook.com>
2025-07-24 01:23:10 -0300

    feat: tagging program arguments (-t, -d)

--- src/cli.js
+++ src/cli.js
@@ -1,22 +1,55 @@
 //Command Line Interface
-import fs from 'fs'
-import errorHandling from './errors/errorFunctions.js'
+import fs from 'fs';
+import path from 'path';
+import errorHandling from './errors/errorFunctions.js';
 import { paragraphBreak } from './index.js';
 import { outFile } from '../helpers.js';
+import { Command } from 'commander';
+import { text } from 'stream/consumers';
+
+const program = new Command();
+
+program
+    .version('0.0.1')
+    .option('-t, --text <string>', 'Path of the text to be processed')
+    .option('-d, --destination <string>', 'Folder path to save the results file')
+    .action((options) => {
+        const { text, destination } = options
+
+        if (!text || !destination) {
+            console.error('error: please enter the origin and destination path');
+            program.help()
+            return;
+        }
+
+        const textPath = path.resolve(text);
+        const destPath = path.resolve(destination);
+
+        try {
+            fileProcess(textPath, destPath)
+            console.log(`Text has been sucefully processed`);
+        } catch (err) {
+            console.log(`An error occurred while processing the file`, err );
+        }
+    })
+
+program.parse();
 
 const filePath = process.argv;
 const link = filePath[2];
 const newFilePath = filePath[3]
 
-fs.readFile(link, 'utf-8', (err, str) => {
-    try {
-        if (err) throw err
-        const result = paragraphBreak(str)
-        saveFile(result, newFilePath)
-    } catch(err) {        
-        errorHandling(err) 
-    }
-})
+function fileProcess(text, destination) {
+    fs.readFile(text, 'utf-8', (err, str) => {
+        try {
+            if (err) throw err
+            const result = paragraphBreak(str)
+            saveFile(result, destination)
+        } catch(err) {        
+            errorHandling(err) 
+        }
+    })
+}
 
 async function saveFile(wordList, path) {
     const newFile = `${path}/result.txt`
